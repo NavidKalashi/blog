c5d98dae66fcca12e3e95729a0b7a76c9647e4ff
NavidKalashi <kalashinavid@gmail.com>
2024-03-08 17:40:30 +0330

    create add function views

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('blogs/', views.blogs, name='blogs'),
     path('blog/<str:pk>/', views.blog, name='blog'),
+    path('add-blog/', views.addBlog, name='add-blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Post, Tag
+from .forms import CustomPostForm
 
 def index(request):
     return render(request, 'index.html')
@@ -14,3 +15,16 @@ def blog(request, pk):
     post = Post.objects.get(id=pk)
     context = {'post': post}
     return render(request, 'blog/blog.html', context)
+
+def addBlog(request):
+    if request.method == 'POST':
+        form = CustomPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blogs')
+    else:
+        form = CustomPostForm()
+    context = {'form': form}
+    return render(request, 'blog/add-blog.html', context)
